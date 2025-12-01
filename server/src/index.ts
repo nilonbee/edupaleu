d89a971c8b83f5ab4d537385f45cd47fc1b26523
nilonbee <nilonbee@gmail.com>
2025-12-01 13:43:44 +0530

    aws configs

--- server/src/index.ts
+++ server/src/index.ts
@@ -59,8 +59,8 @@ app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
 //SERVER
-const port = process.env.PORT || 3001;
+const port = Number(process.env.PORT) || 3001;
 
-app.listen(port, () => {
+app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on port: ${port}`);
 })
