d8949fa7534358f142615bfe343629d1b41934d5
nilonbee <nilonbee@gmail.com>
2025-12-02 20:53:20 +0530

    aws setup

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -23,14 +23,14 @@ const nextConfig = {
     ],
   },
 
-  async rewrites() {
-    return [
-      {
-        source: "/api/v1/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/:path*`,
-      },
-    ];
-  },
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: "/api/v1/:path*",
+  //       destination: `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/:path*`,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
