443a10a0d90fbd97a99890ac61f62826c33c3740
nilonbee <nilonbee@gmail.com>
2025-12-02 21:56:52 +0530

    api base url setup from main

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -154,7 +154,7 @@ export interface Application {
 }
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: '/api/v1/',
+  baseUrl: `${process.env.NEXT_PUBLIC_API_BASE_URL}`,
   credentials: "include", // This ensures cookies are sent with requests
 });
 
