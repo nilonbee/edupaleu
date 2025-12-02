676b7e1d08fcce1975c099188b8d1108e0bc06ca
nilonbee <nilonbee@gmail.com>
2025-12-02 23:10:03 +0530

    s3 bucket url added

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "s3-inventorymanagement.s3.us-east-2.amazonaws.com",
+        hostname: "s3://amzn-s3-bucket-edupaleu",
         port: "",
         pathname: "/**",
       },
